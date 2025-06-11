8debe7e36a4df4b090785d128170da3146ffb130
ElBellaCiao <aguilarvtrading@gmail.com>
2025-06-11 00:41:07 +0000

    export InstanceMetadata

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ mod helper;
 #[cfg(feature = "instance")]
 mod instance;
 #[cfg(feature = "instance")]
-pub use instance::{Ec2, Instance, InstanceId};
+pub use instance::{Ec2, Instance, InstanceId, InstanceMetadata};
 
 #[cfg(feature = "manager")]
 mod manager;
