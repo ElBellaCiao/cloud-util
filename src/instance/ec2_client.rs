9c722930e955f81438a291e50077844e019e9f52
ElBellaCiao <aguilarvtrading@gmail.com>
2025-06-12 02:35:39 +0000

    rename optional argument for ec2_client

--- src/instance/ec2_client.rs
+++ src/instance/ec2_client.rs
@@ -1,10 +1,9 @@
-use crate::InstanceId;
-use crate::helper::aws_client_or_default;
 use crate::instance::InstanceMetadata;
-use anyhow::{Result, anyhow};
-use aws_sdk_ec2::Client;
+use crate::InstanceId;
+use anyhow::{anyhow, Result};
 use aws_sdk_ec2::client::Waiters;
 use aws_sdk_ec2::types::Filter;
+use aws_sdk_ec2::Client;
 use std::collections::HashMap;
 use std::net::IpAddr;
 use std::time::Duration;
@@ -15,8 +14,7 @@ pub struct Ec2Client {
 }
 
 impl Ec2Client {
-    pub async fn new(client: Option<Client>) -> Self {
-        let client = aws_client_or_default(client, Client::new).await;
+    pub async fn new(client: Client) -> Self {
         Self { client }
     }
 }
